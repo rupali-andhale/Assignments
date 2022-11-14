7e79cb5f6c1ef38c0e42f3928951462a16fc0adc
rupali-andhale <rupaliandhale1997@gmail.com>
2022-11-14 17:24:53 +0530

    Hellow World

---
+++ Assignments/src/com/java/assignment1/Message.java
@@ -0,0 +1,14 @@
+package com.java.assignment1;
+
+	// Assignment no 1
+	//Print Hello World massage in console
+public class Message {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hellow World ");
+	}
+
+}
+
+
